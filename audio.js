d6f46d7c829682784e118097be176bc0f9a45193
Sahsquatch <43761408+Sahsquatch@users.noreply.github.com>
2018-11-26 16:45:58 -0500

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,15 @@
+function FilterSample2() {
+  this.isPlaying = false;
+  loadSounds(this, {buffer: 'car drive.wav'});
+};
+
+function FilterSample3() {
+  this.isPlaying = false;
+  loadSounds(this, {buffer: 'sigh copy.wav'});
+};
+
+function FilterSample4() {
+  this.isPlaying = false;
+  loadSounds(this, {buffer: 'shaking jeans copy.wav'});
+};
+
